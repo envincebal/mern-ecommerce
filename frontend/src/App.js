897b552995e2b8afd989a7a215fe91c33fa429ab
envincebal <vincentyan1@gmail.com>
2021-08-06 00:06:57 -0700

    Added images and component folders.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
-import {NavigationBar} from "./components/Navbar";
+import NavigationBar from "./components/Navbar/Navbar";
+import HomePage from "./components/HomePage/HomePage";
+import ProductsPage from "./components/ProductsPage/ProductsPage";
+import ContactPage from "./components/ContactPage/ContactPage";
+import CartPage from "./components/CartPage/CartPage";
+
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <BrowserRouter>
         <NavigationBar />
-      </Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/products" component={ProductsPage} />
+          <Route exact path="/contact" component={ContactPage} />
+          <Route exact path="/cart" component={CartPage} />
+        </Switch>
+      </BrowserRouter>
 
     </div>
   );
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+import "./Navbar.css";
+
+export const NavigationBar = () => {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Link to={"/"}>
+        <Navbar.Brand className="main-title">Tech Store</Navbar.Brand>
+      </Link>
+      <Nav className="mr-auto"></Nav>
+      {/* {user && (  */}
+      <div className="nav-links">
+        <Link to={"/"}>
+          <Button className="link-item" variant="link">
+            Home
+          </Button>
+        </Link>
+        <Link to={"/products"}>
+          <Button className="link-item" variant="link">
+            Products
+          </Button>
+        </Link>
+        <Link to={"/contact"}>
+          <Button className="link-item" variant="link">
+            Contact
+          </Button>
+        </Link>
+        <div className="cart-div">
+        <Link to={"/cart"}>
+
+            <i className="bi bi-cart2 link-item"></i>
+          
+        </Link>
+        <span className="cart-counter">0</span>
+        </div>
+
+        <Link to="/">
+          <Button className="link-item" variant="danger" onClick={() => this.onLogOut()}>
+            Log Out
+          </Button>
+        </Link>
+      </div>
+      {/* // )} */}
+    </Navbar>
+  );
+};
+
+export default NavigationBar;
