e7be304428ba969d09ed4663fbfc4963dbf540ed
envincebal <vincentyan1@gmail.com>
2021-08-14 01:15:39 -0700

    Styled CartPage.

--- frontend/src/components/CartPage/CartPage.jsx
+++ frontend/src/components/CartPage/CartPage.jsx
@@ -1,9 +1,45 @@
 import React from "react";
 
-const CartPage = () => {
+import "./CartPage.css";
+
+const CartPage = (props) => {
   return (
-    <div>
-      <h1>Cart</h1>
+    <div className="cart-page">
+      <h3 className="cart-title">Shopping Cart</h3>
+      <div className="cart-info">
+        <div className="items-container">
+          <div className="items-header">
+            <h5>PRODUCT</h5>
+            <h5>QUANTITY</h5>
+            <h5>PRICE</h5>
+          </div>
+          <div className="item">
+            <p>Product</p>
+            <p>
+              <i class="bi bi-dash-square-fill minus-item"></i>
+              1
+              <i class="bi bi-plus-square-fill add-item"></i>
+            </p>
+            <p>Price</p>
+            <p>Delete</p>
+          </div>
+          <div className="item">
+            <p>Product</p>
+            <p>Quantity</p>
+            <p>Price</p>
+            <p>Delete</p>
+          </div>
+        </div>
+        <div className="order-summary">
+          <h5>Order Summary</h5>
+          <hr />
+          <h6>Items</h6>
+          <p>0</p>
+          <h6>Total Price</h6>
+          <p>$1.00</p>
+          <button>Checkout</button>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/components/ProductDetails/ProductDetails.jsx
+++ frontend/src/components/ProductDetails/ProductDetails.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "./ProductDetails.css";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 const ProductDetails = (props) => {
   const cartCounter = (counter) => {
-    props.setCartCount(counter += 1);
-    console.log(counter);
-  }
+    props.setCartCount((counter += 1));
+  };
 
   const location = useLocation();
   const products = JSON.parse(localStorage.getItem("data"));
-  const productInfo = products.find(item => item.name === location.state.name);
-  const {name, image, price} = productInfo;
-  
+  const productInfo = products.find(
+    (item) => item.name === location.state.name
+  );
+  const { name, image, price } = productInfo;
+
   return (
     <div className="details-container">
       <div className="details-card">
@@ -19,10 +20,12 @@ const ProductDetails = (props) => {
         <div className="details-div">
           <h3 className="details-title">{name}</h3>
           <p className="details-price">${price}</p>
-          <button className="add-to-cart" onClick={() => props.setCartCount(cartCounter)}>
+          <button
+            className="add-to-cart"
+            onClick={() => props.setCartCount(cartCounter)}
+          >
             Add To Cart
           </button>
-          {console.log(useLocation())}
         </div>
       </div>
     </div>
